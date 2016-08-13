f9a444631fc188bf37599e978ea802c9bc75f656
Keroles Hakem <keroleshakem@gmail.com>
2016-08-13 18:15:43 -0400

    add
    add

---
+++ src/BookStore.Db/Scripts/ReferenceData.sql
@@ -0,0 +1,6 @@
+﻿if not exists(select * from dbo.Books where Name = 'Not Started')    
+insert into dbo.Books(Name, Ordinal) values('Not Started', 0); 
+if not exists(select * from dbo.Status where Name = 'In Progress')    
+insert into dbo.Status(Name, Ordinal) values('In Progress', 1); 
+if not exists(select * from dbo.Status where Name = 'Completed')    
+insert into dbo.Status(Name, Ordinal) values('Completed', 2);
