55c4ad8b0baf13cea5f8ee0cb511912b4961de0b
Keroles Hakem <keroleshakem@gmail.com>
2016-10-05 16:51:20 -0400

    - add percentage field to discount.sql

--- src/BookStore.Db/Tables/Discount.sql
+++ src/BookStore.Db/Tables/Discount.sql
@@ -2,10 +2,9 @@
 	[DiscountId] BIGINT IDENTITY (1,1) NOT NULL,
 	[Name] NVARCHAR(100) NOT NULL, 
 	[Description] NVARCHAR(MAX) NOT NULL, 
+	[Percentage] decimal NOT NULL,
     [ValidUntil] DATETIME2 NOT NULL, 
     [CreatedAt] DATETIME2 NOT NULL, 
 	[UpdatedAt] DATETIME2 NOT NULL, 
     [IsEnabled] BIT NOT NULL
-
-
 );
