94a041c81d1d45418a93ec994d08c49130728d90
Keroles Hakem <keroleshakem@gmail.com>
2016-11-01 11:41:14 -0400

    WIP mapping
    with in process of mapping

---
+++ src/BookStore.Data.SqlServer/Mapping/OrderMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using BookStore.Data.Entities;
+namespace BookStore.Data.SqlServer.Mapping
+{
+    public class OrderMap : VersionedClassMap<Order>
+    {
+        public OrderMap()
+        {
+            Id(x => x.OrderId);
+            HasManyToMany(x => x.Books).Access.ReadOnlyPropertyThroughCamelCaseField(Prefix.Underscore);
+
+            Map(x => x.OrderBy).Not.Nullable();
+            Map(x => x.Price).Not.Nullable();
+            Map(x => x.Quantity).Not.Nullable();
+
+        }
+    }
+}
